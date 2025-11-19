29d9bc035f14fa8185ba6806b36bc5cf9239c0c0
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-11-19 11:14:01 -0300

    testando jbutton

--- src/main/java/trabalho/tp2_poo/CorreCabritoGUI.java
+++ src/main/java/trabalho/tp2_poo/CorreCabritoGUI.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package trabalho.tp2_poo;
 
 /**
@@ -30,11 +27,12 @@ public class CorreCabritoGUI extends javax.swing.JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(30, 0), new java.awt.Dimension(30, 0), new java.awt.Dimension(30, 32767));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("CorreCabrito");
@@ -49,27 +47,68 @@ public class CorreCabritoGUI extends javax.swing.JFrame {
         jButton1.setToolTipText("");
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 70, 120, 110));
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 40, 120, 110));
 
-        jButton2.setText("jButton2");
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 250, -1, -1));
-
-        jButton3.setText("jButton3");
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 250, -1, -1));
-
-        jButton4.setText("jButton4");
-        jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 420, -1, -1));
-
-        jButton5.setText("jButton5");
-        jPanel2.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 420, -1, -1));
-
+        jButton6.setBackground(new java.awt.Color(255, 255, 255));
         jButton6.setText("jButton6");
-        jPanel2.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 250, -1, -1));
+        jButton6.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray));
+        jButton6.setContentAreaFilled(false);
+        jButton6.addActionListener(this::jButton6ActionPerformed);
+        jPanel2.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 370, 110, 110));
+
+        jButton7.setBackground(new java.awt.Color(255, 255, 255));
+        jButton7.setText("jButton6");
+        jButton7.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray));
+        jButton7.setContentAreaFilled(false);
+        jButton7.addActionListener(this::jButton7ActionPerformed);
+        jPanel2.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 40, 110, 110));
+
+        jButton8.setBackground(new java.awt.Color(255, 255, 255));
+        jButton8.setText("jButton6");
+        jButton8.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray));
+        jButton8.setContentAreaFilled(false);
+        jButton8.addActionListener(this::jButton8ActionPerformed);
+        jPanel2.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 200, 110, 110));
+
+        jButton9.setBackground(new java.awt.Color(255, 255, 255));
+        jButton9.setText("jButton6");
+        jButton9.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray));
+        jButton9.setContentAreaFilled(false);
+        jButton9.addActionListener(this::jButton9ActionPerformed);
+        jPanel2.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 210, 110, 110));
+
+        jButton10.setBackground(new java.awt.Color(255, 255, 255));
+        jButton10.setText("jButton6");
+        jButton10.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray, java.awt.Color.darkGray));
+        jButton10.setContentAreaFilled(false);
+        jButton10.addActionListener(this::jButton10ActionPerformed);
+        jPanel2.add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 200, 110, 110));
+        jPanel2.add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 320, -1, -1));
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton10ActionPerformed
+
+    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton9ActionPerformed
+
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -97,12 +136,13 @@ public class CorreCabritoGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.Box.Filler filler1;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
